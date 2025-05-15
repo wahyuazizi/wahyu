966d2b43406614fc5abf64d555f038c30e531121
Wahyu Azizi <wahyuazizi03@gmail.com>
2025-05-15 19:41:34 +0800

    add assets and setup navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import React, { useRef } from 'react'
 
-const RotatingCube = () => {
-  const meshRef = useRef()
-  useFrame(() => {
-    if(meshRef.current) {
-      meshRef.current.rotation.x += 0.01
-      meshRef.current.rotation.y += 0.01
-    }
-  })
-  return (
-    <mesh ref={meshRef}>
-      <cylinderGeometry args={[1,1,1]}/>
-      <meshLambertMaterial color="#468585" emissive="#468585" />
-    </mesh>
-  )
-}
-
+import Navbar from './sections/Navbar'
 const App = () => {
-  return(
-    <Canvas style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <OrbitControls enableZoom enablePan enableRotate />
-
-      <directionalLight position={[1,1,1]} instensity={10} color={0x9CDBA6} />
-
-      <color attach="background" args={['#F0F0F0']} />
-
-      <RotatingCube/>
-    </Canvas>
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <Navbar />
+    </main>
   )
 }
 
