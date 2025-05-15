bc9b920df30d0330b4c8031a73e3469091151a34
Wahyu Azizi <wahyuazizi03@gmail.com>
2025-05-15 17:34:48 +0800

    uodate Apps

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import React, { useRef } from 'react'
+
+const RotatingCube = () => {
+  const meshRef = useRef()
+  useFrame(() => {
+    if(meshRef.current) {
+      meshRef.current.rotation.x += 0.01
+      meshRef.current.rotation.y += 0.01
+    }
+  })
+  return (
+    <mesh ref={meshRef}>
+      <cylinderGeometry args={[1,1,1]}/>
+      <meshLambertMaterial color="#468585" emissive="#468585" />
+    </mesh>
+  )
+}
+
+const App = () => {
+  return(
+    <Canvas style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <OrbitControls enableZoom enablePan enableRotate />
+
+      <directionalLight position={[1,1,1]} instensity={10} color={0x9CDBA6} />
+
+      <color attach="background" args={['#F0F0F0']} />
+
+      <RotatingCube/>
+    </Canvas>
+  )
+}
+
+export default App
